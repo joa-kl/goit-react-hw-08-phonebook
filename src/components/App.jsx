3d1d99b79d256afd4ac51b9c897adebe7cbb5d25
Joanna K <joannakleinrm@gmail.com>
2023-07-14 23:08:20 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,8 @@ import { ContactList } from "./ContactList/ContactList";
 import { Filter } from "./Filter/Filter";
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
+import { LoginPage } from 'pages/LoginPage';
+import { RegisterForm } from 'pages/RegisterForm';
 
   const App = () => {
 
@@ -13,7 +15,9 @@ import { Layout } from './Layout/Layout';
         <Routes>
           <Route path="/" element={<Layout/>} > 
             <Route index element={<HomePage />} />
-            {/* <Route path="/" element={<HomePage />} /> */}
+            <Route path="login" element={<LoginPage />} />
+            <Route path="register" element={<RegisterForm />} />
+           
           </Route>
         </Routes>
       </div>
