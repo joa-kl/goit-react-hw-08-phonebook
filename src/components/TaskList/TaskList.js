f160704daafedad849256025a3ac215f44c85de3
Joanna K <joannakleinrm@gmail.com>
2023-07-15 17:12:15 +0200

    contacts redux update

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,18 +1,18 @@
 import { useSelector } from 'react-redux';
 import { Task } from '../Task/Task';
-import { selectAllTasks } from '../../redux/tasks/selectors';
+import { selectAllTasks } from '../../redux/contacts/selectors';
 // import css from './TaskList.module.css';
 
 export const TaskList = () => {
   const tasks = useSelector(selectAllTasks);
 
-    return (
-      <ul>
-        {tasks.map(({ id, text }) => (
-          <li key={id}>
-            <Task id={id} text={text} />
-          </li>
-        ))}
-      </ul>
+  return (
+    <ul>
+      {tasks.map(({ id, text }) => (
+        <li key={id}>
+          <Task id={id} text={text} />
+        </li>
+      ))}
+    </ul>
   )
 };
