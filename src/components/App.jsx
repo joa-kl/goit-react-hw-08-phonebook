281ef7de0943f4e985d6ea3469c3d3029b973070
Joanna K <joannakleinrm@gmail.com>
2023-07-15 15:37:24 +0200

    pages update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,24 @@
-import { HomePage } from 'pages/HomePage';
+// import { HomePage } from 'pages/Home';
 // import css from './App.module.css';
 // import ContactForm  from "./ContactForm/ContactForm";
 // import { ContactList } from "./ContactList/ContactList";
 // import { Filter } from "./Filter/Filter";
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
-import { LoginPage } from 'pages/LoginPage';
-import { RegisterPage } from 'pages/RegisterPage';
+// import { LoginPage } from 'pages/Login';
+// import { RegisterPage } from 'pages/Register';
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { lazy, useEffect } from 'react';
 import { PrivateRoute } from './PrivateRoute/PrivateRoute';
 import { RestrictedRoute } from './RestrictedRoute/RestrictedRoute';
-import { TasksPage } from 'pages/TasksPage';
+// import { TasksPage } from 'pages/Tasks';
 import { useAuth } from 'hooks/useAuth';
 import { refreshUser } from 'redux/auth/operations';
+
+const HomePage = lazy(() => import('../pages/Home'));
+const RegisterPage = lazy(() => import('../pages/Register'));
+const LoginPage = lazy(() => import('../pages/Login'));
+const TasksPage = lazy(() => import('../pages/Tasks'));
 
 export const App = () => {
   const dispatch = useDispatch();
