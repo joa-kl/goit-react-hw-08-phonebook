8f6b933bd80435b7099a4972ef4844a857994fbc
Joanna K <joannakleinrm@gmail.com>
2023-07-19 22:27:25 +0200

    styles update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,11 @@
-// import ContactForm from "components/ContactForm/ContactForm"
-// import { ContactList } from "components/ContactList/ContactList"
-// import { Filter } from "components/Filter/Filter";
-
-const styles = {
-    container: {
-        // height: '100%',
-        // display: 'flex',
-        alignItems: 'center',
-        paddingTop: '100px',
-        justifyContent: 'center',
-        // backgroundColor: 'teal'
-    },
-    title: {
-        fontWeight: 500,
-        fontSize: 40,
-        textAlign: 'center',
-    },
-    subtitle: {
-        fontWeight: 400,
-        fontSize: 18,
-        textAlign: 'center',
-    }
-};
+import css from "./Home.module.css";
 
 
 const Home = () => {
     return (
-        <div style={styles.container}>
-            <h1 style={styles.title}>Welcome to your phonebook app!</h1>
-            <p style={styles.subtitle}>The easiest access to all your contacts. Just log in or register!</p>
+        <div className={css.container}>
+            <h1 className={css.title}>Welcome to your phonebook app!</h1>
+            <p className={css.subtitle}>The easiest access to all your contacts. Just log in or register!</p>
         </div>
     );
 }
